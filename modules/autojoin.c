c4cbe8087d215888c39226b5a499280f90eba04e
Jacek Galowicz <jacek.galowicz@rwth-aachen.de>
2013-01-11 17:35:07 +0100

    Added an autojoin module

---
+++ modules/autojoin.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+#include "module.h"
+#include "_module_init.c.inc"
+#include "../config.h"
+#include "../helpers.h"
+
+static char *channels = NULL;
+
+int module_message_handler(irc_connection *con, irc_msg *msg)
+{
+	if (strcmp(msg->command, "001")) return 0;
+
+	Irc_send(con, "JOIN %s\n", channels);
+
+	return 1;
+}
+
+int module_init(irc_connection *con)
+{
+	channels = Conf("channels", "");
+
+	if (!strlen(channels)) return 1;
+
+	return 0;
+}
+
+void module_close(void) 
+{
+}
